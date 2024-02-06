c7d557828069d7f02afc5ba65fc89458394659f7
Rom <romka@monday.com>
2024-02-07 00:57:21 +0200

    remove unused

--- packages/api/rollup.config.js
+++ packages/api/rollup.config.js
@@ -1,13 +1,13 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json'; // Import the plugin
 
 // Determine if we are in a production environment
 // const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/index.ts', // Your main TypeScript entry point
+  input: 'lib/index.ts', // Your main TypeScript entry point
   output: [
     {
       file: 'dist/cjs/index.js',
@@ -19,17 +19,12 @@ export default {
       format: 'es', // ES module, for bundlers
       sourcemap: true,
     },
-    {
-      file: 'dist/umd/your-library.js',
-      name: 'YourLibrary',
-      format: 'umd', // UMD, for browser
-      sourcemap: true,
-    },
   ],
   plugins: [
     resolve(), // tells Rollup how to find external modules
     commonjs(), // converts CommonJS modules to ES6
     typescript(), // compiles TypeScript to JavaScript
+    json(),
     // production && terser(), // minify, but only in production
   ],
 };
