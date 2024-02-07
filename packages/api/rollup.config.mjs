2ed1b7037910d1c7a2853d3e83d44e35bbfa22a3
Rom <romka@monday.com>
2024-02-07 10:50:21 +0200

    change config

--- packages/api/rollup.config.mjs
+++ packages/api/rollup.config.mjs
@@ -1,8 +1,32 @@
-// rollup.config.mjs
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json'; // Import the plugin
+
+// Determine if we are in a production environment
+// const production = !process.env.ROLLUP_WATCH;
+
 export default {
-  input: 'lib/index.ts',
-  output: {
-    file: 'bundle.js',
-    format: 'cjs',
-  },
+  input: 'lib/index.ts', // Your main TypeScript entry point
+  output: [
+    {
+      file: 'dist/cjs/index.js',
+      format: 'cjs', // CommonJS, for Node, could also be 'amd', 'esm', 'iife', 'umd'
+      sourcemap: true,
+    },
+    {
+      file: 'dist/esm/index.js',
+      format: 'es', // ES module, for bundlers
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    resolve(), // tells Rollup how to find external modules
+    commonjs(), // converts CommonJS modules to ES6
+    typescript({ tsconfig: './tsconfig.json' }), // compiles TypeScript to JavaScript
+    json(),
+    // production && terser(), // minify, but only in production
+  ],
+
+  external: ['graphql-request', 'graphql-tag'],
 };
