b06ab9c1a68c97bd5118869843f1abd4ebaae5ab
Rom <romka@monday.com>
2024-02-07 20:24:20 +0200

    use sdk

--- packages/api/lib/api-client.ts
+++ packages/api/lib/api-client.ts
@@ -1,10 +1,10 @@
 import { GraphQLClient } from 'graphql-request';
 import { defaultVersion } from './constants';
-import { getSdk } from './generated/sdk';
+import { Sdk, getSdk } from './generated/sdk';
 
 export class ApiClient {
   private readonly client: GraphQLClient;
-  public readonly operations: Record<string, any> = {};
+  public readonly operations: Sdk;
 
   constructor(token: string, apiVersion = defaultVersion) {
     this.client = new GraphQLClient('https://api.monday.com/v2', {
@@ -12,7 +12,6 @@ export class ApiClient {
         'Content-Type': 'application/json',
         Authorization: token.toString(),
         'API-Version': apiVersion.toString(),
-        'User-Agent': 'monday-sdk-js',
       },
     });
 
