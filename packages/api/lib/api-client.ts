06151a0a968147b41b8f5b14a1906e0900d3f055
Rom <romka@monday.com>
2024-02-08 17:43:47 +0200

    default versions

--- packages/api/lib/api-client.ts
+++ packages/api/lib/api-client.ts
@@ -1,12 +1,12 @@
 import { GraphQLClient } from 'graphql-request';
-import { defaultVersion } from './constants';
+import { AvailableVersions, defaultVersion } from './constants';
 import { Sdk, getSdk } from './generated/sdk';
 
 export class ApiClient {
   private readonly client: GraphQLClient;
   public readonly operations: Sdk;
 
-  constructor(token: string, apiVersion = defaultVersion) {
+  constructor(token: string, apiVersion: AvailableVersions | string = defaultVersion) {
     this.client = new GraphQLClient('https://api.monday.com/v2', {
       headers: {
         'Content-Type': 'application/json',
--- packages/api/lib/constants.ts
+++ packages/api/lib/constants.ts
@@ -1,7 +1,6 @@
 export enum AvailableVersions {
-  STABLE = '2024-01',
-  PREVIEW = '2024-04',
-  NEXT = '2024-07',
+  STABLE = 'stable',
+  PREVIEW = 'preview',
 }
 
 export const defaultVersion = AvailableVersions.STABLE;
