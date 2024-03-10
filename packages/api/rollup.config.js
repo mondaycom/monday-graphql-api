cda70feaac9dbed850874ea856c16e2c1fa610d6
Rom <romka@monday.com>
2024-03-10 13:38:52 +0200

    force rebuild

--- packages/api/rollup.config.js
+++ packages/api/rollup.config.js
@@ -3,8 +3,10 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import json from '@rollup/plugin-json';
+import del from 'rollup-plugin-delete';
+import dts from 'rollup-plugin-dts';
 
-export default {
+const mainConfig = {
   input: 'lib/index.ts',
   output: [
     {
@@ -18,7 +20,25 @@ export default {
       sourcemap: true,
     },
   ],
-  plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), json(), terser()],
+  plugins: [
+    del({ targets: 'dist/*' }),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    json(),
+    terser(),
+  ],
 
   external: ['graphql-request', 'graphql-tag'],
 };
+
+const typesConfig = {
+  input: 'lib/index.ts',
+  output: [
+    { file: 'dist/cjs/index.d.ts', format: 'es' },
+    { file: 'dist/esm/index.d.ts', format: 'es' },
+  ],
+  plugins: [dts()],
+};
+
+export default [mainConfig, typesConfig];
