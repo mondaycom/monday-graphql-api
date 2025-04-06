718ff6f431b06c5ecf5218d4514c5d4084bb0973
Rom <romka@monday.com>
2025-04-06 14:44:44 +0300

    move to yarn

--- packages/monday-api-mcp/jest.config.js
+++ packages/monday-api-mcp/jest.config.js
@@ -2,18 +2,14 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  extensionsToTreatAsEsm: ['.ts'],
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
         useESM: true,
-        tsconfig: 'tsconfig.test.json',
       },
     ],
   },
+  extensionsToTreatAsEsm: ['.ts'],
   testMatch: ['**/tests/**/*.test.ts'],
 };
