b6c9befddd27f47424da790b0dfa1f5d52b8c26e
Rom <romka@monday.com>
2025-04-15 19:15:49 +0300

    update version

--- packages/api/lib/constants/index.ts
+++ packages/api/lib/constants/index.ts
@@ -4,7 +4,7 @@ export enum AvailableVersions {
   CURRENT = 'current',
   RELEASE_CANDIDATE = 'release_candidate',
   DEV = 'dev',
-  CURRENT_VERSION = '2025-01',
+  CURRENT_VERSION = '2025-04',
 }
 
 export const MONDAY_API_ENDPOINT = 'https://api.monday.com/v2';
